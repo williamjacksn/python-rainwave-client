06efbc310ce2998d9f8e2c0fbbf72f61fa8fe20b
William Jackson <william@subtlecoolness.com>
2013-06-18 15:07:01 -0500

    Change some properties of RainwaveSong to return datetime objects

--- song.py
+++ song.py
@@ -1,20 +1,18 @@
 import artist
+import datetime
 
 
 class RainwaveSong(object):
 
     simple_properties = [
-        u'addedon',
         u'available',
         u'favourite',
         u'id',
-        u'lastplayed',
         u'rating_avg',
         u'rating_count',
         u'rating_id',
         u'rating_sid',
         u'rating_user',
-        u'releasetime',
         u'secondslong',
         u'timesdefeated',
         u'timesplayed',
@@ -33,24 +31,32 @@ class RainwaveSong(object):
     def __getattr__(self, name):
         if name in self.simple_properties:
             if u'song_{}'.format(name) not in self._raw_info:
-                for song in self.album.songs:
-                    if song.id == self.id:
-                        self._merge_raw_info(song)
+                self._extend()
             return self._raw_info[u'song_{}'.format(name)]
         else:
             raise AttributeError
 
+    def _extend(self):
+        for song in self.album.songs:
+            if song.id == self.id:
+                self._merge_raw_info(song)
+
     def _merge_raw_info(self, other):
         self._raw_info = dict(self._raw_info.items() + other._raw_info.items())
+
+    @property
+    def addedon(self):
+        if u'song_addedon' not in self._raw_info:
+            self._extend()
+        ts = self._raw_info[u'song_addedon']
+        return datetime.datetime.utcfromtimestamp(ts)
 
     @property
     def artists(self):
         if not hasattr(self, u'_artists'):
             self._artists = []
             if u'artists' not in self._raw_info:
-                for song in self.album.songs:
-                    if song.id == self.id:
-                        self._merge_raw_info(song)
+                self._extend()
             for raw_artist in self._raw_info[u'artists']:
                 channel = self.album._channel
                 new_artist = artist.RainwaveArtist(channel, raw_artist)
@@ -66,8 +72,22 @@ class RainwaveSong(object):
         return self.favourite
 
     @property
+    def lastplayed(self):
+        if u'song_lastplayed' not in self._raw_info:
+            self._extend()
+        ts = self._raw_info[u'song_lastplayed']
+        return datetime.datetime.utcfromtimestamp(ts)
+
+    @property
     def rating_channel_id(self):
         return self.rating_sid
+
+    @property
+    def releasetime(self):
+        if u'song_releasetime' not in self._raw_info:
+            self._extend()
+        ts = self._raw_info[u'song_releasetime']
+        return datetime.datetime.utcfromtimestamp(ts)
 
     @property
     def sid(self):
