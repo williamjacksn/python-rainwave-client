dda91ae23ece88abebc3ff9dbbaf163d05ceaf39
William Jackson <william@subtlecoolness.com>
2013-06-26 12:36:31 -0500

    Simplify namespace

--- gutter/__init__.py
+++ gutter/__init__.py
@@ -1,1 +1,6 @@
 from .client import RainwaveClient
+from .channel import RainwaveChannel
+from .album import RainwaveAlbum
+from .artist import RainwaveArtist
+from .cooldown import RainwaveCooldown
+from .song import RainwaveSong
