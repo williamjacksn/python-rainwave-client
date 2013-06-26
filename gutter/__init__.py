db86a492bd36c805bdcaf83e69192f36ec5ce9e7
William Jackson <william@subtlecoolness.com>
2013-06-26 12:38:17 -0500

    Use correct class name

--- gutter/__init__.py
+++ gutter/__init__.py
@@ -2,5 +2,5 @@ from .client import RainwaveClient
 from .channel import RainwaveChannel
 from .album import RainwaveAlbum
 from .artist import RainwaveArtist
-from .cooldown import RainwaveCooldown
+from .cooldown import RainwaveCooldownGroup
 from .song import RainwaveSong
