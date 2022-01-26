d9e14d3a687c4b81d08e9a4b6021ee6831d34367
William Jackson <william@subtlecoolness.com>
2022-01-26 14:22:39 -0600

    Release 0.8.0

--- rainwaveclient/__init__.py
+++ rainwaveclient/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 __setup_name__ = 'python-rainwave-client'
-__version__ = '0.7.0'
+__version__ = '0.8.0'
 __author__ = 'William Jackson'
 __author_email__ = 'william@subtlecoolness.com'
 __url__ = 'https://github.com/williamjacksn/python-rainwave-client'
