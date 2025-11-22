da463947f0756b9e6cbcbfdf42efe453e30bbb83
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-11-22 16:44:47 -0600

    Dependabot weekly instead of daily

--- ci/gen.py
+++
@@ -1,8 +0,0 @@
-import json
-import pathlib
-
-
-def gen(content: dict, target: str):
-    pathlib.Path(target).write_text(
-        json.dumps(content, indent=2, sort_keys=True), newline="\n"
-    )
